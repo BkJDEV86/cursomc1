196c12a54754a0c17c3eb3c394b100a8ba62b43b
Bruno Kappel Jordao <brunokappeljordao@yahoo.com.br>
2022-02-03 19:13:48 -0300

    Criando operação de instanciação

--- src/main/java/com/nelioalves/cursomc1/Cursomc1Application.java
+++ src/main/java/com/nelioalves/cursomc1/Cursomc1Application.java
@@ -1,13 +1,34 @@
 package com.nelioalves.cursomc1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Cursomc1Application {
+import com.nelioalves.cursomc1.domain.Categoria;
+import com.nelioalves.cursomc1.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Cursomc1Application implements CommandLineRunner { // Aqui é para poder ser lançado os objetos diretamento no Banco de Dados
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
