85c02793a94c83cfe5454c05400ccf8139a8e8c0
Bruno Kappel Jordao <brunokappeljordao@yahoo.com.br>
2022-02-12 12:25:31 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/nelioalves/cursomc1/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc1/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.nelioalves.cursomc1.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc1.domain.Categoria;
+import com.nelioalves.cursomc1.dto.CategoriaDTO;
 import com.nelioalves.cursomc1.services.CategoriaService;
 
 @RestController
@@ -61,9 +64,18 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 			
-		
 	}
 	
+	@RequestMapping( method=RequestMethod.GET) // aqui não vai ter value pois irá retonar todas as categorias
+	public 	ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		//Aqui acima estamos convertendo uma lista para outra lista, transformando uma stream de objetos para uma outra lista.
+		
+		return ResponseEntity.ok().body(listDto);
 	
-
-}
+	
+	
+	}
+	}
--- src/main/java/com/nelioalves/cursomc1/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc1/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc1.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,5 +48,9 @@ public class CategoriaService {
 		
 	   }
 	
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
