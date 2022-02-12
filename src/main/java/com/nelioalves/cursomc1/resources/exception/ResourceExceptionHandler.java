966e1ead9f59500b16b5ec41fed94b4fa423c19e
Bruno Kappel Jordao <brunokappeljordao@yahoo.com.br>
2022-02-12 11:58:30 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/nelioalves/cursomc1/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc1/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc1.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc1.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice // Gerenciador de erros. // classe auxiliar que vai receptar as exceções tendo a assinatura abaixo
@@ -22,4 +23,15 @@ public class ResourceExceptionHandler {
 		
 	}
 
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		
+		// aqui modificamos de not_found para bad_request pois é outro tipo de exceção...
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	
+	
+	}
+	
+	
 }
--- src/main/java/com/nelioalves/cursomc1/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc1/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.nelioalves.cursomc1.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc1.domain.Categoria;
 import com.nelioalves.cursomc1.repositories.CategoriaRepository;
+import com.nelioalves.cursomc1.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc1.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,7 +19,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
+			return obj.orElseThrow(() -> new ObjectNotFoundException(
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	
@@ -36,6 +38,14 @@ public class CategoriaService {
 		
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+	    } catch (DataIntegrityViolationException e) {
+		       throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		
+	   }
 	
-
+	}
 }
