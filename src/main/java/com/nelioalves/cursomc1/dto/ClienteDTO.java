a6a3684a8576727638261c19cfa5c24d8c754a0e
Bruno Kappel Jordao <brunokappeljordao@yahoo.com.br>
2022-02-17 22:15:28 -0300

    PUT, DELETE e GET para Clientes

---
+++ src/main/java/com/nelioalves/cursomc1/dto/ClienteDTO.java
@@ -0,0 +1,67 @@
+package com.nelioalves.cursomc1.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.nelioalves.cursomc1.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Size(min=5,max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Email(message="Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+		
+		
+		
+	}
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/nelioalves/cursomc1/resources/ClienteResource.java
+++ src/main/java/com/nelioalves/cursomc1/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.nelioalves.cursomc1.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc1.domain.Cliente;
+import com.nelioalves.cursomc1.dto.ClienteDTO;
 import com.nelioalves.cursomc1.services.ClienteService;
 
 @RestController
@@ -30,6 +39,55 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj); // Aqui é pra dizer que ocorreu tudo bem com o objeto.
 		
 	}
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)// Usamos essa anotação igual a da classe Response acima pois
+	//quando colocarmos a uri no postman precisamos chamar a url com o id
+	// Aqui abaixo é uma mistura chamando o corpo da requisição e o inteiro
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDto);// converter para objeto...
+		obj.setId(id);
+		obj=service.update(obj);
+		return ResponseEntity.noContent().build();// Aqui não tem que retornar nenhum contéudo por isso é noContent e o código no
+		// postman é 204
+		
+	}
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.DELETE) 
+	public 	ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+			
+	}
+	
+	@RequestMapping( method=RequestMethod.GET) // aqui não vai ter value pois irá retonar todas as categorias
+	public 	ResponseEntity<List<ClienteDTO>> findAll() {
+		
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		//Aqui acima estamos convertendo uma lista para outra lista, transformando uma stream de objetos para uma outra lista.
+		
+		return ResponseEntity.ok().body(listDto);
+	
+	
+	
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET) // aqui não vai ter value pois irá retonar todas as categorias
+	public 	ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue="0") Integer page,
+		    @RequestParam(value = "linesPerPage", defaultValue="24")Integer linesPerPage,
+		    @RequestParam(value = "orderBy", defaultValue="nome") String orderBy,
+		    @RequestParam(value = "direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		//Aqui acima estamos convertendo uma lista para outra lista, transformando uma stream de objetos para uma outra lista.
+		
+		return ResponseEntity.ok().body(listDto);
+	
+	
+	
+	}
+	
+	
 	
 	
 	
